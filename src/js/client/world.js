2693695316e0748037275f8314b236406388e56f
cmarkwell <markwell.connor@gmail.com>
2020-08-03 19:59:36 -0400

    Move THREE code into new class

---
+++ src/js/client/world.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three';
+
+export default class World {
+    #canvas
+    #scene;
+    #camera;
+    #renderer;
+
+    constructor(canvas) {
+        this.#canvas = document.querySelector(canvas);
+    }
+    
+    init = () => {
+        const { innerWidth, innerHeight } = this.#canvas;
+
+        // Set up scene, main camera, and renderer
+        this.#scene = new THREE.Scene();
+        this.#camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight);
+        this.#renderer = new THREE.WebGLRenderer({ canvas: this.#canvas });
+
+        // Window even listeners
+        window.addEventListener('resize', this.onWindowResize, false);
+    }
+
+    onWindowResize = () => {
+        const { innerWidth, innerHeight } = this.#canvas;
+
+        // Redraw canvas with updated aspect ratio and size
+        this.#camera.aspect = innerWidth / innerHeight;
+        this.#camera.updateProjectionMatrix();
+        this.#renderer.setSize(innerWidth, innerHeight);
+    }
+
+    animate = () => {
+        requestAnimationFrame(this.animate);
+        this.#renderer.render(this.#scene, this.#camera);
+    }
+}
